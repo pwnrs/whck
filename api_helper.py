789852bcb0a4edc9b75df1ac23709d5bbe29d651
jeremymuhia <jmuhia80@gmail.com>
2017-12-11 23:04:32 -0500

    make helper function

---
+++ api_helper.py
@@ -0,0 +1,16 @@
+import os
+import requests
+
+base_yelp_url = 'https://api.yelp.com/v3/businesses/search?'
+headers = {'Authorization': 'Bearer ' + os.environ['YELP_API_KEY']}
+params = {'term': 'food', 'radius': 20000}
+
+def get_food_at_location(location):
+    if invalid(location):
+        return None
+    params['location'] = location
+    resp = requests.get(base_yelp_url, params=params, headers=headers)
+    return resp
+
+def invalid(value):
+    return value == None or value == '' or type(value) != str
